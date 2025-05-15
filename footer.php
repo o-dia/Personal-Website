dd692f6621b06f6b61dcc2ce4b4465fdf4121ec7
Omar Dia <o.dia@nyu.edu>
2025-05-15 15:27:05 +0100

    added garmin and github links to footer

--- footer.php
+++ footer.php
@@ -6,6 +6,9 @@
                 <div class="social-links">
                     <a href="https://www.linkedin.com/in/omardia/" target="_blank" class="social-icon linkedin">LinkedIn</a>
                     <a href="https://x.com/babaghanoushbb" target="_blank" class="social-icon x-social">X</a>
+                    <a href="https://instagram.com/o.dia" target="_blank" class="social-icon instagram">instagram</a>
+                    <a href="https://connect.garmin.com/modern/profile/63ffb8f8-1450-40c4-8df0-1c41b02febb5" target="_blank" class="social-icon garmin">garmin</a>
+                    <a href="https://github.com/o-dia" target="_blank" class="social-icon github">github</a>
                     <!-- Add more social links as needed -->
                 </div>
                 
